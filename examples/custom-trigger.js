1f5aa02206774e4c4e64f7b8fe4e0a5a344f80e2
clevernyyyy <adam.t.schaal@gmail.com>
2018-09-18 10:00:58 -0500

    Updates

---
+++ examples/custom-trigger.js
@@ -0,0 +1,56 @@
+webpackJsonp([1],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(18);
+
+
+/***/ }),
+
+/***/ 18:
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-color-picker/assets/index.less\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(9);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	var _rcColorPicker = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-color-picker\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _rcColorPicker2 = _interopRequireDefault(_rcColorPicker);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function changeHandler(colors) {
+	  console.log(colors);
+	}
+	
+	_reactDom2.default.render(_react2.default.createElement(
+	  'div',
+	  { style: { textAlign: 'center' } },
+	  _react2.default.createElement(
+	    _rcColorPicker2.default,
+	    {
+	      color: '#36c',
+	      onChange: changeHandler
+	    },
+	    _react2.default.createElement(
+	      'span',
+	      { className: 'react-custom-trigger' },
+	      'choose color'
+	    )
+	  )
+	), document.getElementById('__react-content'));
+
+/***/ })
+
+});
+//# sourceMappingURL=custom-trigger.js.map
---
+++ examples/panel.js
@@ -0,0 +1,45 @@
+webpackJsonp([2],{
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(19);
+
+
+/***/ }),
+
+/***/ 19:
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-color-picker/assets/index.less\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	var _react = __webpack_require__(2);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	var _reactDom = __webpack_require__(9);
+	
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+	
+	var _rcColorPicker = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"rc-color-picker\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function onChange(obj) {
+	  console.log(obj);
+	}
+	
+	_reactDom2.default.render(_react2.default.createElement(
+	  'div',
+	  { style: { backgroundColor: '#ddd', padding: 20 } },
+	  _react2.default.createElement(_rcColorPicker.Panel, { enableAlpha: false, color: '#345679', onChange: onChange, mode: 'RGB' }),
+	  _react2.default.createElement('hr', null),
+	  _react2.default.createElement(_rcColorPicker.Panel, { alpha: 80, color: '#477898', onChange: onChange, mode: 'HSB' })
+	), document.getElementById('__react-content'));
+
+/***/ })
+
+});
+//# sourceMappingURL=panel.js.map
